9085032915c2f0dbdf51d55142271470cb744230
peterchijioke <peterchijioke1@gmail.com>
2021-12-29 19:08:01 +0100

    aftj

--- App.js
+++ App.js
@@ -4,13 +4,28 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 import Home from './Screens/Home'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Home" component={Home}
+       options={{
+        headerTitle: '',
+        headerTintColor: '#000',
+        headerLeft: () => (
+          <Icon.Button
+            name="arrow-back"
+            size={25}
+            backgroundColor="#fff"
+            color="#000"
+            onPress={() => navigation.goBack()}
+          />
+        ),
+      }}
+      />
       </Stack.Navigator>
     </NavigationContainer>
   )
